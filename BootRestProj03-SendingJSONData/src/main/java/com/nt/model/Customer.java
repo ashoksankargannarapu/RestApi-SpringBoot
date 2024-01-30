10c94d8388f54337eded49c74d801d461011a9dc
ashok <ashok@gmail.com>
2024-01-30 14:58:33 +0530

    Send the JSON data

---
+++ BootRestProj03-SendingJSONData/src/main/java/com/nt/model/Customer.java
@@ -0,0 +1,17 @@
+package com.nt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+	
+	private Integer cno;
+	private String cname;
+	private String location;
+	private Float billamt;
+
+}
