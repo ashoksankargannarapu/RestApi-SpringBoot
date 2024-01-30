ba495f58631698f56f3d22465ebd325a5f64d1b6
ashok <ashok@gmail.com>
2024-01-30 15:00:51 +0530

    JSON to Collections

---
+++ BootRestProj07-JSONTOCollectionsObjectsUsingRequestBody/src/main/java/com/nt/BootRestProj07JsontoCollectionsObjectsUsingRequestBodyApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestProj07JsontoCollectionsObjectsUsingRequestBodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestProj07JsontoCollectionsObjectsUsingRequestBodyApplication.class, args);
+	}
+
+}
